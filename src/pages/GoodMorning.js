42fa5e1876dc3e87b689cb2582d1d071ddbf6707
Anastasia <168160374+AnastasiaZubrilovskaia@users.noreply.github.com>
2026-03-26 10:32:11 +0400

    nn

---
+++ src/pages/GoodMorning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GoodMorning() {
+  return (
+    <div>
+      <h1>Доброе утро!</h1>
+      <p>Сегодня 26 марта</p>
+    </div>
+  );
+}
+
+export default GoodMorning;
